11155057c1a2188c9f9ec688af2ac8a489df7223
decl5th <alsgur6624@gmail.com>
2024-08-08 10:08:31 +0900

    8/7 리액트 연동 관련 변동사항

--- src/main/java/com/jmt/member/entities/Authorities.java
+++ src/main/java/com/jmt/member/entities/Authorities.java
@@ -1,5 +1,6 @@
 package com.jmt.member.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.jmt.member.constants.Authority;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -15,6 +16,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Authorities {
     @Id
+    @JsonIgnore // json 변환시에 문제 발생 처리
     @ManyToOne(fetch= FetchType.LAZY)
     private Member member;
 
