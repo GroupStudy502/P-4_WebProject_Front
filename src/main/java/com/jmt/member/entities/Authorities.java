8e45f28f8ec7e41216e07b90a0ea800a724202f3
sswum <l5450won@naver.com>
2024-08-08 10:14:22 +0900

    순환참조 오류로 인해서 변경

--- src/main/java/com/jmt/member/entities/Authorities.java
+++ src/main/java/com/jmt/member/entities/Authorities.java
@@ -1,5 +1,6 @@
 package com.jmt.member.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.jmt.member.constants.Authority;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -15,11 +16,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Authorities {
     @Id
-    @ManyToOne(fetch= FetchType.LAZY)
-    private Member member;
+    @JsonIgnore
+@ManyToOne(fetch= FetchType.LAZY)
+private Member member;
 
-    @Id
-    @Column(length=20)
-    @Enumerated(EnumType.STRING)
-    private Authority authority;
+@Id
+@Column(length=20)
+@Enumerated(EnumType.STRING)
+private Authority authority;
 }
