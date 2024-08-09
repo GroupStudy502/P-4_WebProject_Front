28bf747fe6252edeefede7e58cf8e974aeddf71c
decl5th <alsgur6624@gmail.com>
2024-08-09 19:47:52 +0900

    DB 업데이트 서비스 구현 중 한글 깨짐 현상 -> rest 패키지에서 매핑으로 현상 해결 시도 Restaurant entity build 작업 중 에러 발생

--- src/main/java/com/jmt/restaurant/services/DataTransferService.java
+++ src/main/java/com/jmt/restaurant/services/DataTransferService.java
@@ -1,5 +1,7 @@
 package com.jmt.restaurant.services;
 
+import com.jmt.global.rests.gov.api.ApiResult;
+import com.jmt.restaurant.entities.Restaurant;
 import com.jmt.restaurant.repositories.FoodMenuImageRepository;
 import com.jmt.restaurant.repositories.FoodMenuRepository;
 import com.jmt.restaurant.repositories.RestaurantImageRepository;
@@ -11,6 +13,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.List;
+import java.util.Map;
 
 @Lazy
 @Service
@@ -22,19 +26,84 @@ public class DataTransferService {
     private final FoodMenuImageRepository foodMenuImageRepository;
 
     private final RestTemplate restTemplate;
-    private String serviceKey = "2Mrt1R9eHQpvh5el660cDj0zfFIKSwZHyA8rDjCc2Cm9nKrcbWZ3p2faMLBbOY5f";
 
+    private String serviceKey = "2Mrt1R9eHQpvh5el660cDj0zfFIKSwZHyA8rDjCc2Cm9nKrcbWZ3p2faMLBbOY5f";
 
     /**
      * 식당 기본 정보
      */
     public void update1() {
-        String url = String.format("http://seoul.openapi.redtable.global/api/rstr?serviceKey=%s", serviceKey);
+        String url = String.format("https://seoul.openapi.redtable.global/api/rstr?serviceKey=%s", serviceKey);
 
-        ResponseEntity<String> response = restTemplate.getForEntity(URI.create(url), String.class);
-        System.out.println(response);
+        ResponseEntity<ApiResult> response = restTemplate.getForEntity(URI.create(url), ApiResult.class);
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            return;
+        }
+
+        ApiResult result = response.getBody();
+        if (!result.getHeader().get("resultCode").equals("00")) {
+            return;
+        }
+        List<Map<String, String>> tmp = result.getBody();
+
+        String url2 = String.format("https://seoul.openapi.redtable.global/api/rstr/oprt?serviceKey=%s", serviceKey);
+        ResponseEntity<ApiResult> result2 = restTemplate.getForEntity(URI.create(url2), ApiResult.class);
+
+        List<Map<String, String>> tmp2 = result2.getBody().getBody();
+
+        List<Restaurant> items = tmp.stream()
+                .map(d -> {
+                    Map<String, String> extra = getExtra(tmp2, d.get("RSTR_ID"));
+                    Restaurant rest = Restaurant.builder()
+                        .rstrId(Long.valueOf(d.get("RSTR_ID")))
+                        .rstrNm(d.get("RSTR_NM"))
+                        .rstrRdnmAdr(d.get("RSTR_RDNMADR"))
+                        .rstrLnnoAdres(d.get("RSTR_LNNO_ADRES"))
+                        .rstrLa(Double.valueOf(d.get("RSTR_LA")))
+                        .rstrLo(Double.valueOf(d.get("RSTR_LO")))
+                        .rstrTelNo(d.get("RSTR_TELNO"))
+                        .dbsnsStatmBzcndNm(d.get("DBSNS_STATM_BZCND_NM"))
+                        .bsnsLcncNm(d.get("BSNS_LCNC_NM"))
+                        .rstrIntrcnCont(d.get("RSTR_INTRCN_CONT"))
+                        .build();
+
+                    if (extra == null) {
+                        return rest;
+                    }
+
+                    rest.setAreaNm(extra.get("AREA_NM"));
+                    if (extra.get("PRDL_SEAT_CNT") != null) rest.setPrdlSeatCnt(Integer.valueOf(extra.get("PRDL_SEAT_CNT")));
+                    if(extra.get("SEAT_CNT") != null) rest.setSeatCnt(Integer.valueOf(extra.get("SEAT_CNT")));
+                    rest.setPrkgPosYn(d.get("PRKG_POS_YN").equals("Y"));
+                    rest.setWifiOfrYn(d.get("WIFI_OFR_YN").equals("Y"));
+                    rest.setDcrnYn(d.get("DCRN_YN").equals("Y"));
+                    rest.setPetEntrnPosblYn(d.get("PET_ENTRN_POSBL_YN").equals("Y"));
+                    rest.setFgggMenuOfrYn(d.get("FGGG_MENU_OFR_YN").equals("Y"));
+                    rest.setTlromInfoCn(d.get("TLROM_INFO_CN"));
+                    rest.setRestdyInfoCn(d.get("RESTDY_INFO_CN"));
+                    rest.setBsnsTmCn(d.get("BSNS_TM_CN"));
+                    rest.setHmdlvSaleYn(d.get("HMDLV_SALE_YN").equals("Y"));
+                    rest.setDsbrCvntlYn(d.get("DSBR_CVNTL_YN").equals("Y"));
+                    rest.setDelvSrvicYn(d.get("DELV_SRVIC_YN").equals("Y"));
+                    rest.setRsrvMthdNm(d.get("RSRV_MTHD_NM"));
+                    rest.setOnlineRsrvInfoCn(d.get("ONLINE_RSVD_INFO_CN"));
+                    rest.setHmpgUrl(d.get("HMPG_URL"));
+                    rest.setKioskYn(d.get("KIOSK_YN").equals("Y"));
+                    rest.setMbPmamtYn(d.get("MB_PMAMT_YN").equals("Y"));
+                    rest.setSmorderYn(d.get("SMORDER_YN").equals("Y"));
+                    rest.setReprsntMenuNm(d.get("REPRSNT_MENU_NM"));
+
+                    return rest;
+                }).toList();
+
     }
 
+    private Map<String, String> getExtra(List<Map<String, String>> items, String rstrId){
+        if (items == null || items.isEmpty()) return null;
 
-
+        return items.stream()
+                .filter(d-> d.get("RSTR_ID").equals(rstrId))
+                .findFirst()
+                .orElse(null);
+    }
 }
