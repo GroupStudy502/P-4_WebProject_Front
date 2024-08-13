4ff4ff2753fc67361f0d58310965a34d308af954
decl5th <alsgur6624@gmail.com>
2024-08-13 19:31:13 +0900

    식당 조회 1. 서치 데이터에 페이지 데이터 존재 2. 검색 처리 3. 검색 데이터 처리 -> 기본 검색 4. 페이징 데이터

--- src/main/java/com/jmt/restaurant/services/RestaurantInfoService.java
+++ src/main/java/com/jmt/restaurant/services/RestaurantInfoService.java
@@ -1,17 +1,58 @@
 package com.jmt.restaurant.services;
 
+import com.jmt.global.ListData;
+import com.jmt.global.Pagination;
+import com.jmt.restaurant.controllers.RestaurantSearch;
+import com.jmt.restaurant.entities.QRestaurant;
 import com.jmt.restaurant.entities.Restaurant;
 import com.jmt.restaurant.repositories.RestaurantRepository;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class RestaurantInfoService {
     private final RestaurantRepository repository; // 카운트 할 때 필요
-    private final RestaurantInfoService infoService;
+    private final JPAQueryFactory queryFactory;
+
+    public ListData<Restaurant> getList(RestaurantSearch search) {
+        int page = Math.max(search.getPage(), 1); // 페이지가 0이거나 음수이면 1이 나오도록 설정
+        int limit = search.getLimit(); // 한페이지당 보여줄 레코드 개수
+        limit = limit < 1 ? 20 : limit;
+        int offset = (page -1) * limit; // 레코드 시작 위치 구하기
+
+        // 검색 처리 B
+        String sopt = search.getSopt(); // 검색 옵션 All - 통합 검색
+        String skey = search.getSkey();  // 검색 키워드를 통한 검색 ex) 음식분류, 옵션 검색
+
+        QRestaurant restaurant = QRestaurant.restaurant;
+        BooleanBuilder andBuilder = new BooleanBuilder();
+
+        // 검색 처리 D
+
+        // 검색 데이터 처리
+        List<Restaurant> items = queryFactory.selectFrom(restaurant)
+                .leftJoin(restaurant.images)
+                .fetchJoin()
+                .where(andBuilder) // 검색 조건 후에 추가
+                .offset(offset)
+                .limit(limit)
+                .orderBy(restaurant.createdAt.desc()) // 정렬 조건 후에 추가
+                .fetch();
+
+        // 페이징 데이터
+        long total = repository.count(andBuilder); // 조회된 전체 갯수
+
+        Pagination pagination = new Pagination(page, (int)total, 10, limit);
+
+        return new ListData<>(items, pagination);
+    }
 
 
 
