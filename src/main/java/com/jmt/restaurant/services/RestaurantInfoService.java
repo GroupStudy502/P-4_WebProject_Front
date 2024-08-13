ab5d1985b5b482c62c109ee204621ce2b921adbf
decl5th <alsgur6624@gmail.com>
2024-08-13 19:13:20 +0900

    쿼리를 직접 작성해야하는 경우를 예상하여 beanconfig에 의존성 주입

--- src/main/java/com/jmt/restaurant/services/RestaurantInfoService.java
+++ src/main/java/com/jmt/restaurant/services/RestaurantInfoService.java
@@ -9,7 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class RestaurantInfoService {
-    private final RestaurantRepository restaurantRepository;
+    private final RestaurantRepository restaurantRepository; // 카운트 할 때 필요
+
 
 
 }
