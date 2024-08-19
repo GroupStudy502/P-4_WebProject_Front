de2b6f580c364089fad571ab185050fe129d03ae
lionjeongin <cindy93489163@gmail.com>
2024-08-19 14:53:00 +0900

    결제 페이지 Order 엔티티 작성

---
+++ src/main/generated/com/jmt/order/entities/QOrderItem.java
@@ -0,0 +1,46 @@
+package com.jmt.order.entities;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QOrderItem is a Querydsl query type for OrderItem
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QOrderItem extends EntityPathBase<OrderItem> {
+
+    private static final long serialVersionUID = -1179438818L;
+
+    public static final QOrderItem orderItem = new QOrderItem("orderItem");
+
+    public final com.jmt.global.entities.QBaseEntity _super = new com.jmt.global.entities.QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> deletedAt = _super.deletedAt;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    public QOrderItem(String variable) {
+        super(OrderItem.class, forVariable(variable));
+    }
+
+    public QOrderItem(Path<? extends OrderItem> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QOrderItem(PathMetadata metadata) {
+        super(OrderItem.class, metadata);
+    }
+
+}
+
