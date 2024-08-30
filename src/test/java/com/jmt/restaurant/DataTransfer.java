2d5a966bd7fc9109529b12df97aa2554e1501cc0
jek730 <jejek730@gmail.com>
2024-08-30 10:50:28 +0900

    공공 데이터 API - 식당 품질 정보 추가

--- src/test/java/com/jmt/restaurant/DataTransfer.java
+++ src/test/java/com/jmt/restaurant/DataTransfer.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
@@ -38,7 +39,7 @@ public class DataTransfer {
 
     @Test
     @DisplayName("식당 이미지 정보")
-     void update2() {
+    void update2() {
         for (int i = 1; i <= 10; i++) {
             service.update2(i);
         }
@@ -46,7 +47,6 @@ public class DataTransfer {
 
     @Test
     @DisplayName("메뉴 기본 정보")
-
     void update3() {
         for (int i = 501; i<= 600; i++) {
             service.update3(i);
