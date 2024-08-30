a953e2d4a4af78df401d34f89f6630f12dba2bca
hjchoirr <hjchoir@naver.com>
2024-08-30 14:09:36 +0900

    Reservation admin

--- src/main/java/com/jmt/reservation/entities/Reservation.java
+++ src/main/java/com/jmt/reservation/entities/Reservation.java
@@ -1,5 +1,6 @@
 package com.jmt.reservation.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.jmt.global.entities.BaseEntity;
 import com.jmt.member.entities.Member;
