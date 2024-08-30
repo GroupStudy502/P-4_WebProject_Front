6eb907352a57fdebfb9d3ad671935a1f27b20df1
lionjeongin <cindy93489163@gmail.com>
2024-08-30 16:37:11 +0900

    예약 데이터 수정 -> 다른 데이터 불러오기

--- src/main/java/com/jmt/reservation/entities/Reservation.java
+++ src/main/java/com/jmt/reservation/entities/Reservation.java
@@ -1,7 +1,6 @@
 package com.jmt.reservation.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.jmt.global.entities.BaseEntity;
 import com.jmt.member.entities.Member;
 import com.jmt.payment.constants.PayMethod;
@@ -27,12 +26,10 @@ public class Reservation extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private ReservationStatus status = ReservationStatus.START;
 
-    @JsonIgnore
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     private Member member;
 
-    @JsonIgnore
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="rstrId")
     private Restaurant restaurant;
 
